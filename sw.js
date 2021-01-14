a29842b66f9042240026d3e7e34bbb9d69046af4
uniquesalman786 <43309438+uniquesalman786@users.noreply.github.com>
2021-01-14 12:34:34 +0530

    done 1

---
+++ sw.js
@@ -0,0 +1,44 @@
+// On install - caching the application shell
+var CACHE_NAME = 'demo-cache';
+var urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+  
+  // On network request
+ self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
